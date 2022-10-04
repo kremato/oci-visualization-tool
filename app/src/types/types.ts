552a856f579c2d55131b87a4862f83ce9a4475a4
Jakub Krajčík <jakubkrajcik1@gmail.com>
2022-10-04 19:16:38 +0200

    style: styles added

--- app/src/types/types.ts
+++ app/src/types/types.ts
@@ -1,4 +1,4 @@
 import * as identity from "oci-identity";
 
 export type Compartment = identity.models.Compartment
-export type HierarchyMap = Map<string, Compartment>;
+export type HierarchyMap = Map<string, Compartment[]>;
