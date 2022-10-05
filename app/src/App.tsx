84cc180e725aee2d402032ee9705e558a0ce512d
Jakub Krajčík <jakubkrajcik1@gmail.com>
2022-10-05 15:00:04 +0200

    refactor: no error in App useEffect

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,15 +1,18 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { getCompartmentsList } from "./store/compartmentsActionCreators";
+import { useAppDispatch } from "./hooks/useAppDispatch";
+import { useAppSelector } from "./hooks/useAppSelector";
+import { getCompartmentsList, getHierarchyMap } from "./store/compartmentsActionCreators";
 
 function App() {
-  const dispatch = useDispatch();
+  const dispatch = useAppDispatch();
+  const compartmets = useAppSelector((state) => state.compartments.compartmentsList)
 
   useEffect(() => {
     dispatch(getCompartmentsList());
-  }, [dispatch]);
+    dispatch(getHierarchyMap(compartmets));
+  }, [dispatch, compartmets]);
 
-  return <div>{``}</div>;
+  return <div>AHOJ</div>;
 }
 
 export default App;
