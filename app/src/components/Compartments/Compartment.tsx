62734a494867aebfa1f044992a57dcea33448d2d
Jakub Krajčík <jakubkrajcik1@gmail.com>
2022-10-09 21:48:26 +0200

    refactor: compartment list now works as it should

--- app/src/components/Compartments/Compartment.tsx
+++ app/src/components/Compartments/Compartment.tsx
@@ -1,21 +1,9 @@
 import { useAppSelector } from "../../hooks/useAppSelector";
-import ListSubheader from "@mui/material/ListSubheader";
-import List from "@mui/material/List";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
-import Collapse from "@mui/material/Collapse";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
-import DraftsIcon from "@mui/icons-material/Drafts";
-import SendIcon from "@mui/icons-material/Send";
-import ExpandLess from "@mui/icons-material/ExpandLess";
-import ExpandMore from "@mui/icons-material/ExpandMore";
-import StarBorder from "@mui/icons-material/StarBorder";
-import { useState } from "react";
-import { CollapsableWrapper } from "./CollapsableWrapper";
+import RemoveIcon from "@mui/icons-material/Remove";
 import AddIcon from "@mui/icons-material/Add";
-import RemoveIcon from '@mui/icons-material/Remove';
-import { ListItem } from "@mui/material";
+import { IconButton, ListItem, ListItemText } from "@mui/material";
+import { CollapsableWrapper } from "./CollapsableWrapper";
+import { useState } from "react";
 
 interface Props {
   id: string;
@@ -40,24 +28,29 @@ export const Compartment = ({ depth, id, name }: Props) => {
       ? hierarchyHash[hierarchyHash[id][0].id]
       : hierarchyHash[id];
 
+  const icon = open ? <RemoveIcon /> : <AddIcon />;
+
   // it name is undefined, it means this is this rootId
   // compartment and that is why there is one element in children,
   // and that is the root compartment
   if (!name) {
     name = hierarchyHash[id][0].name;
   }
+
   const handleClick = () => {
     setOpen(!open);
   };
 
-  const icon = open ? <RemoveIcon /> : <AddIcon />
-
   return (
     <>
-      <ListItem sx={{ pt: 0, pr: 0, pb: 0, pl: depth*2 }} onClick={children && handleClick}>
-        <ListItemIcon sx={{ minWidth: "10%" }}>
+      <ListItem sx={{ pt: 0, pr: 0, pb: 0, pl: depth * 2 }}>
+        <IconButton
+          sx={{ p: 0, minWidth: "10%" }}
+          aria-label={open ? "minimize" : "expand"}
+          onClick={handleClick}
+        >
           {children && icon}
-        </ListItemIcon>
+        </IconButton>
         <ListItemText
           primary={name}
           secondary={currentId}
