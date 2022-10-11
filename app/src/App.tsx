1a0ab524b57886996d6c39b6247ab444425501e0
Jakub Krajčík <jakubkrajcik1@gmail.com>
2022-10-11 11:05:24 +0200

    style

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,11 +1,8 @@
 import { useEffect } from "react";
-import Bar from "./components/Compartments/Bar";
 import { CompartmentList } from "./components/Compartments/CompartmentList";
-import { Foo } from "./components/Compartments/Foo";
 import { useAppDispatch } from "./hooks/useAppDispatch";
 import { fetchCompartmentsList } from "./store/compartmentsActionCreators";
 import "./App.css";
-import VirtualizedList from "./components/Compartments/VirtualizedList";
 
 function App() {
   const dispatch = useAppDispatch();
